3f9b098991811821d0bac93f18ee60509d6bf164
ArunKumar406-hub <arundeepakkumar.mylavarapu@slu.edu>
2025-11-30 17:06:23 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,3 +9,20 @@ function App() {
 }
 
 export default App;
+
+import { Route, Routes} from 'react-router-dom';
+
+// ...
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      
+        <Routes>
+          <Route path="/" element={<CardList data={productData} />} />
+          <Route path="/product/:id" element={<SingleView data={productData} />} />
+        </Routes>
+      
+    </div>
+  );
+}
