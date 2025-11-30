3c1bb1748bb0b2b55f7cfef9ad504489f69865c4
ArunKumar406-hub <arundeepakkumar.mylavarapu@slu.edu>
2025-11-30 16:55:52 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,3 +9,21 @@ function App() {
 }
 
 export default App;
+import CardList from './components/CardList';
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <CardList />
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <CardList data={productData} />
+    </div>
+  );
+}
